f2bcc4662eae5149367b40e88c8c25a21c653fc5
Naoki Kitamura <qclove00@NaokinoMacBook-Pro.local>
2024-10-15 16:53:25 +0900

    rdm

--- examples/ccsd_1rdm_mod.py
+++ examples/ccsd_1rdm_mod.py
@@ -3,7 +3,7 @@ from wick.index import Idx
 from wick.operator import FOperator, Tensor
 from wick.expression import Term, Expression, AExpression
 from wick.wick import apply_wick
-from wick.convenience import E1, E2, commute, ketE1
+from wick.convenience import E1, E2, commute, ketE1, ketE2
 
 i = Idx(0, "occ")
 a = Idx(0, "vir")
@@ -21,17 +21,27 @@ L = L1 + L2
 # ov block
 operators = [FOperator(a, True), FOperator(i, False)]
 pvo = Expression([Term(1, [], [Tensor([i, a], "")], operators, [])])
-ket = ketE1("occ", "vir")
+ket1 = ketE1("occ", "vir")
+ket2 = ketE2("occ", "vir", "occ", "vir")
 
 PT = commute(pvo, T)
 PTT = commute(PT, T)
 mid = pvo + PT + Fraction('1/2')*PTT
-full = L*mid*ket
-out = apply_wick(full)
-out.resolve()
-final = AExpression(Ex=out)
-print("P_{ov} = ")
+full1 = L*mid*ket1
+out1 = apply_wick(full1)
+out1.resolve()
+final = AExpression(Ex=out1)
+print("P_{ov}1_ia = ")
 print(final)
+print(final._print_einsum())
+
+full1 = L*mid*ket2
+out1 = apply_wick(full1)
+out1.resolve()
+final = AExpression(Ex=out1)
+print("P_{ov}2_ia = ")
+print(final)
+print(final._print_einsum())
 
 # vv block
 operators = [FOperator(a, True), FOperator(b, False)]
@@ -40,13 +50,23 @@ pvv = Expression([Term(1, [], [Tensor([b, a], "")], operators, [])])
 PT = commute(pvv, T)
 PTT = commute(PT, T)
 mid = pvv + PT + Fraction('1/2')*PTT
-full = L*mid
+full = L*mid*ket1
 out = apply_wick(full)
 out.resolve()
 final = AExpression(Ex=out)
 final.sort_tensors()
-print("P_{vv} = ")
+print("P_{vv}1_ab = ")
 print(final)
+print(final._print_einsum())
+
+full = L*mid*ket2
+out = apply_wick(full)
+out.resolve()
+final = AExpression(Ex=out)
+final.sort_tensors()
+print("P_{vv}2_ab = ")
+print(final)
+print(final._print_einsum())
 
 # oo block
 operators = [FOperator(j, False), FOperator(i, True)]
@@ -55,13 +75,24 @@ poo = Expression([Term(-1, [], [Tensor([j, i], "")], operators, [])])
 PT = commute(poo, T)
 PTT = commute(PT, T)
 mid = poo + PT + Fraction('1/2')*PTT
-full = L*mid
+full = L*mid*ket1
 out = apply_wick(full)
 out.resolve()
 final = AExpression(Ex=out)
 final.sort_tensors()
-print("P_{oo} = ")
+print("P_{oo}1_ij = ")
 print(final)
+print(final._print_einsum())
+
+full = L*mid*ket2
+out = apply_wick(full)
+out.resolve()
+final = AExpression(Ex=out)
+final.sort_tensors()
+print("P_{oo}2_ij = ")
+print(final)
+print(final._print_einsum())
+
 
 # vo block
 operators = [FOperator(i, True), FOperator(a, False)]
@@ -70,10 +101,20 @@ pvo = Expression([Term(1, [], [Tensor([a, i], "")], operators, [])])
 PT = commute(pvo, T)
 PTT = commute(PT, T)
 mid = pvo + PT + Fraction('1/2')*PTT
-full = mid + L*mid
+full = (mid + L*mid) * ket1
 out = apply_wick(full)
 out.resolve()
 final = AExpression(Ex=out)
 final.sort_tensors()
-print("P_{vo} = ")
+print("P_{vo}1_ia = ")
 print(final)
+print(final._print_einsum())
+
+full = (mid + L*mid) * ket2
+out = apply_wick(full)
+out.resolve()
+final = AExpression(Ex=out)
+final.sort_tensors()
+print("P_{vo}2_ia = ")
+print(final)
+print(final._print_einsum())
